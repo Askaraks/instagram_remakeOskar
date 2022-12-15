69121096f400e1c3cdce469b800e14d6dd06ab50
Askaraks <askar.mamadaliev@gmail.com>
2022-12-15 17:36:59 +0600

    переделал

--- app/src/main/java/com/asashka/instagrama/LakeFragment.java
+++ app/src/main/java/com/asashka/instagrama/LakeFragment.java
@@ -14,7 +14,7 @@ public class LakeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_lake2, container, false);
+        return inflater.inflate(R.layout.fragment_like, container, false);
     }
 
     @Override
